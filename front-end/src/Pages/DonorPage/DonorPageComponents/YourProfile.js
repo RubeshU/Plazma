935abcbc2f46723cbcc2dec64c41507e8afa158e
RubeshU <rubeshstark53@gmail.com>
2022-02-12 16:29:22 +0000

    donor your profile page completed

--- front-end/src/Pages/DonorPage/DonorPageComponents/YourProfile.js
+++ front-end/src/Pages/DonorPage/DonorPageComponents/YourProfile.js
@@ -13,18 +13,18 @@ const YourProfile = () => {
             
             <h1 style={{textAlign:"center",color: "#761A19"}}> Sudarsan M S</h1>
 
-            <Container className="profile_data">
+            <Container className="profile_data d-flex justify-content-center mt-5 mb-5">
                 <Row>
-                    <Col align="center"> 
+                    <Col align="center" className="profile_data_1"> 
                         AGE
                     </Col>
-                    <Col align="center">
+                    <Col align="center" className="profile_data_2">
                         20
                     </Col>
                 </Row>
             </Container>
-            
         </Container>
+        
     );
 }
 
