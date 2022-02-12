684caa6e103994ebc5ee98fc1b8459684beae653
RubeshU <rubeshstark53@gmail.com>
2022-02-12 15:49:18 +0300

    sudarsan commit1

--- front-end/src/Pages/DonorPage/DonorPageComponents/DonorSection.js
+++ front-end/src/Pages/DonorPage/DonorPageComponents/DonorSection.js
@@ -4,8 +4,8 @@ import { Container,Row,Col } from "react-bootstrap";
 
 const DonorSection = () => {
     return (
-        <div style={{ backgroundColor: "#FCEEF0" }}>
-            <Container>
+        <div style={{ backgroundColor: "#FCEEF0" ,padding: "5rem"}}>
+            <Container className="pb-5">
                 <Row>
                     <Col md={6} sm={12} lg={4} align="center" style={{ paddingTop: "5rem", paddingBottom: "2rem" }}>
                         <NavLink
--- front-end/src/Pages/DonorPage/DonorPageComponents/YourProfile.js
+++ front-end/src/Pages/DonorPage/DonorPageComponents/YourProfile.js
@@ -1,6 +1,30 @@
+import "./YourProfile.css";
+import ProfilePic from "../DonorAssets/ProfilePic.png";
+import { Container, Image, Row ,Col} from "react-bootstrap";
+
 const YourProfile = () => {
     return (
-        <h1 style={{textAlign: "center",paddingBottom: "3rem",marginBottom: "0rem"}}>Your Profile Page</h1>
+        <Container className="profile_card">
+            <Row>
+                <Col lg={12} align="center">
+                    <Image fluid src={ProfilePic} />
+                </Col>
+            </Row>
+            
+            <h1 style={{textAlign:"center",color: "#761A19"}}> Sudarsan M S</h1>
+
+            <Container className="profile_data">
+                <Row>
+                    <Col align="center"> 
+                        AGE
+                    </Col>
+                    <Col align="center">
+                        20
+                    </Col>
+                </Row>
+            </Container>
+            
+        </Container>
     );
 }
 
